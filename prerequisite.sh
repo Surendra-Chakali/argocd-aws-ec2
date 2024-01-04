b160dec63e7c6c79a3184bd6fb816fef4d2e8560
Ubuntu <ubuntu@ip-10-0-16-229.eu-west-2.compute.internal>
2024-01-04 07:56:10 +0000

    Initial commit

---
+++ prerequisite.sh
@@ -0,0 +1,21 @@
+sudo apt update
+
+sudo apt install docker.io -y
+sudo usermod -aG docker $USER
+sudo newgrp docker
+
+
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
+sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+minikube start --driver docker --force
+
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl port-forward svc/argocd-server -n argocd --address 0.0.0.0 8080:443
